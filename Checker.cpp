5b3be2f5d924e64dc1c0b5b4175d7920c4e8902d
SahanaSuresh2412 <sahanasuresh.2412@gmail.com>
2024-08-30 13:53:35 +0530

    Update Checker.cpp

--- Checker.cpp
+++ Checker.cpp
@@ -2,76 +2,73 @@
 #include <cassert>
 #include <string>
 
+using namespace std;
+
 // Pure function to check temperature
-bool checkTemperature(float temperature, std::string &message) 
-{
-    if (temperature < 0 || temperature > 45) {
-        message = "Temperature out of range!";
+bool checkTemperature(float temperature, std::string &message) {
+    if (temperature < 0) {
+        message = "Temperature too low!";
+        return false;
+    } else if (temperature > 45) {
+        message = "Temperature too high!";
         return false;
     }
     return true;
 }
 
 // Pure function to check state of charge (SOC)
-bool batteryIsOk(float temperature, float soc, float chargeRate, std::string &message) 
-{
-    bool temperatureOk = checkTemperature(temperature, message);
-    bool socOk = checkSOC(soc, message);
-    bool chargeRateOk = checkChargeRate(chargeRate, message);
-
-    if (!temperatureOk) {
+bool checkSOC(float soc, std::string &message) {
+    if (soc < 20) {
+        message = "State of Charge too low!";
+        return false;
+    } else if (soc > 80) {
+        message = "State of Charge too high!";
         return false;
     }
-    if (!socOk) {
-        return false;
-    }
-    if (!chargeRateOk) {
-        return false;
-    }
-
-    message = "Battery is OK.";
     return true;
 }
 
 // Pure function to check charge rate
-bool checkChargeRate(float chargeRate, std::string &message)
-{
+bool checkChargeRate(float chargeRate, std::string &message) {
     if (chargeRate > 0.8) {
-        message = "Charge Rate out of range!";
+        message = "Charge Rate too high!";
         return false;
     }
     return true;
 }
 
-// Function that uses pure functions and adds I/O
-bool batteryIsOk(float temperature, float soc, float chargeRate, std::string &message) 
-{
-    if ((!checkTemperature(temperature, message)) && (!checkSOC(soc, message)) && (!checkChargeRate(chargeRate, message))) 
-    {
+// Main function combining checks and I/O
+bool batteryIsOk(float temperature, float soc, float chargeRate, std::string &message) {
+    if (!checkTemperature(temperature, message) || !checkSOC(soc, message) || !checkChargeRate(chargeRate, message)) {
         return false;
     }
     message = "Battery is OK.";
     return true;
 }
 
+// Test function
 void testBatteryIsOk() {
     std::string message;
 
+    // Test case where battery is OK
     assert(batteryIsOk(25, 70, 0.7, message) == true);
     assert(message == "Battery is OK.");
 
-    // assert(batteryIsOk(50, 85, 0, message) == false);
-    // assert(message == "Temperature out of range!");
+    // Test case where temperature is too high
+    assert(batteryIsOk(50, 70, 0.7, message) == false);
+    assert(message == "Temperature too high!");
 
-    assert(batteryIsOk(25, 85, 0, message) == false);
-    assert(message == "State of Charge out of range!");
+    // Test case where SOC is too low
+    assert(batteryIsOk(25, 10, 0.7, message) == false);
+    assert(message == "State of Charge too low!");
 
+    // Test case where charge rate is too high
     assert(batteryIsOk(25, 70, 0.9, message) == false);
-    assert(message == "Charge Rate out of range!");
+    assert(message == "Charge Rate too high!");
 }
 
 int main() {
     testBatteryIsOk();
-    std::cout << "All tests passed.\n";
+    cout << "All tests passed.\n";
     return 0;
 }
